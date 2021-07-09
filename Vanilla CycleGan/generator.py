ad39f23b92187848ea74b79c18aa5b6ce5178434
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2021-07-09 18:20:10 +0530

    add discriminator

---
+++ Vanilla CycleGan/generator.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, down=True, use_act=True, **kwargs):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, padding_mode="reflect", **kwargs)
+            if down
+            else nn.ConvTranspose2d(in_channels, out_channels, **kwargs),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU() if use_act else nn.Identity(),
+        )
