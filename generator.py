778639a114610d5ce2f8105aef629df746e3e55b
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2021-08-09 11:28:04 +0530

    complete generator

--- generator.py
+++ generator.py
@@ -1,17 +1,19 @@
 import torch
 import torch.nn as nn
+import selfAttentionBlock
 
 
 class ResidualBlock(nn.Module):
     def __init__(self, channels):
         super().__init__()
         self.block = nn.Sequential(
+            # I am not sure which res block to take
             nn.Conv2d(
                 in_channels=256,
                 out_channels=256,
                 kernel_size=1,
                 stride=1,
-                padding=1,
+                padding=0,
             )
             # nn.Conv2d(
             #     in_channels=256,
@@ -30,7 +32,7 @@ class Generator(nn.Module):
     def __init__(self, img_channels):
         super().__init__()
 
-        downConvBlock = nn.Sequential(
+        self.downConvBlock = nn.Sequential(
             nn.Conv2d(
                 in_channels=img_channels,
                 out_channels=1,
@@ -42,7 +44,7 @@ class Generator(nn.Module):
                 in_channels=1,
                 out_channels=64,
                 kernel_size=6,
-                stride=1,
+                stride=2,
                 padding=2,
                 bias=False,
             ),
@@ -70,49 +72,76 @@ class Generator(nn.Module):
             nn.ReLU(),
         )
 
-        upConvBlock = nn.Sequential(
-            nn.ConvTranspose2d(
-                in_channels=256,
-                out_channels=128,
-                kernel_size=3,
-                stride=2,
-                padding=1,
-                bias=False,
-            ),
-            nn.InstanceNorm2d(128),
-            nn.ReLU(),
-            nn.ConvTranspose2d(
-                in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1
-            ),
-            nn.ConvTranspose2d(
-                in_channels=64, out_channels=1, kernel_size=6, stride=2, padding=2
-            ),
-            nn.Conv2d(
-                in_channels=1,
-                out_channels=1,
-                kernel_size=7,
-                stride=1,
-                padding=3,
-            ),
-            nn.Tanh(),
+        self.convtrans1 = nn.ConvTranspose2d(
+            in_channels=256,
+            out_channels=128,
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            bias=False,
         )
 
+        self.active1 = nn.Sequential(nn.InstanceNorm2d(128), nn.ReLU())
+
+        self.convtrans2 = nn.ConvTranspose2d(
+            in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1
+        )
+
+        self.convtrans3 = nn.ConvTranspose2d(
+            in_channels=64, out_channels=1, kernel_size=6, stride=2, padding=2
+        )
+
+        self.lastlayer = nn.Sequential(
+            # nn.ConvTranspose2d(
+            #     in_channels=256,
+            #     out_channels=128,
+            #     kernel_size=3,
+            #     stride=2,
+            #     padding=1,
+            #     bias=False,
+            # ),
+            # # nn.InstanceNorm2d(128),
+            # # nn.ReLU(),
+            # # nn.ConvTranspose2d(
+            # #     in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1
+            # # ),
+            # # nn.ConvTranspose2d(
+            # #     in_channels=64, out_channels=1, kernel_size=6, stride=2, padding=2
+            # # ),
+            # nn.Conv2d(
+            #     in_channels=1,
+            #     out_channels=1,
+            #     kernel_size=7,
+            #     stride=1,
+            #     padding=3,
+            # ),
+            # nn.Tanh(),
+        )
+
+        self.selfAttentionlayer = selfAttentionBlock.SelfAttentionBlock(in_dim=256)
+        self.resBlock1 = ResidualBlock(channels=256)
+        self.resBlock2 = ResidualBlock(channels=256)
+
     def forward(self, x):
-        x = self.initial(x)
-        for layer in self.down_blocks:
-            x = layer(x)
-        x = self.residual_blocks(x)
-        for layer in self.up_blocks:
-            x = layer(x)
-        return torch.tanh(self.last(x))
+        batch_size, ch, h, w = x.shape
+        out = self.downConvBlock(x)
+        out = self.resBlock1(out)
+        out, _ = self.selfAttentionlayer(out)
+        out = self.resBlock2(out)
+        out = self.convtrans1(out, output_size=(batch_size, 128, 32, 32))
+        out = self.active1(out)
+        out = self.convtrans2(out, output_size=(batch_size, 64, 64, 64))
+        out = self.convtrans3(out, output_size=(batch_size, 1, 128, 128))
+        out = self.lastlayer(out)
+        return out
 
 
 def test():
-    img_channels = 3
-    img_size = 256
+    img_channels = 1
+    img_size = 128
     x = torch.randn(2, img_channels, img_size, img_size)
     gen = Generator(img_channels)
-    # print(gen)
+    print(gen)
     print(gen(x).shape)
 
 
--- selfAttentionBlock.py
+++ selfAttentionBlock.py
@@ -59,7 +59,7 @@ class SelfAttentionBlock(nn.Module):
             in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
         )
         self.value_conv = nn.Conv2d(
-            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+            in_channels=in_dim, out_channels=in_dim, kernel_size=1
         )
 
         self.gamma = nn.Parameter(torch.zeros(1))
@@ -76,21 +76,36 @@ class SelfAttentionBlock(nn.Module):
         """
 
         batch_size, c, h, w = x.shape
+
         proj_query = self.query_conv(x).view(batch_size, -1, w * h).permute(0, 2, 1)
+
         # proj_query with view is of shape (batch_size, c, w*h)
         # proj_query with permute becomes batch_size, w*h, c), ie transpose of proj_query for matrix multiplication
         proj_key = self.key_conv(x).view(batch_size, -1, w * h)  # (batch_size, c, w*h)
+
         energy = torch.bmm(proj_query, proj_key)  # (batch_size, w*h, w*h)
+
         attention = self.softmax(energy)  # (batch_size, w*h, w*h)
 
         proj_value = self.value_conv(x).view(
             batch_size, -1, w * h
         )  # (batch_size, c*8, w*h) c*8 = C
+
         out = torch.bmm(
             proj_value, attention.permute(0, 2, 1)
         )  # (batch_size, c*8, w*h)
+
         # attention is transposed for matrix mul
         out = out.view(batch_size, c, w, h)
 
         out = self.gamma * out + x
         return out, attention
+
+
+def testFunc():
+    x = torch.randn(3, 256, 16, 16)
+    simpleModel = SelfAttentionBlock(in_dim=256)
+    print(simpleModel(x)[0].shape)
+
+
+testFunc()
