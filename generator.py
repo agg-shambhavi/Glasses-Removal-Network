863bb51b0bff2e1bf7be5f9c1c2240f133e7023f
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2021-08-09 09:11:52 +0530

    modify generator

--- generator.py
+++ generator.py
@@ -2,27 +2,24 @@ import torch
 import torch.nn as nn
 
 
-class ConvBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, down=True, use_act=True, **kwargs):
-        super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, padding_mode="reflect", **kwargs)
-            if down
-            else nn.ConvTranspose2d(in_channels, out_channels, **kwargs),
-            nn.InstanceNorm2d(out_channels),
-            nn.ReLU() if use_act else nn.Identity(),
-        )
-
-    def forward(self, x):
-        return self.conv(x)
-
-
 class ResidualBlock(nn.Module):
     def __init__(self, channels):
         super().__init__()
         self.block = nn.Sequential(
-            ConvBlock(channels, channels, kernel_size=3, padding=1),
-            ConvBlock(channels, channels, use_act=False, kernel_size=3, padding=1),
+            nn.Conv2d(
+                in_channels=256,
+                out_channels=256,
+                kernel_size=1,
+                stride=1,
+                padding=1,
+            )
+            # nn.Conv2d(
+            #     in_channels=256,
+            #     out_channels=256,
+            #     kernel_size=7,
+            #     stride=1,
+            #     padding=3,
+            # )
         )
 
     def forward(self, x):
@@ -30,69 +27,74 @@ class ResidualBlock(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self, img_channels, num_features=64, num_residuals=9):
+    def __init__(self, img_channels):
         super().__init__()
-        self.initial = nn.Sequential(
+
+        downConvBlock = nn.Sequential(
             nn.Conv2d(
-                img_channels,
-                num_features,
+                in_channels=img_channels,
+                out_channels=1,
                 kernel_size=7,
                 stride=1,
                 padding=3,
-                padding_mode="reflect",
             ),
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=64,
+                kernel_size=6,
+                stride=1,
+                padding=2,
+                bias=False,
+            ),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=256,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.InstanceNorm2d(256),
             nn.ReLU(),
         )
 
-        self.down_blocks = nn.ModuleList(
-            [
-                ConvBlock(
-                    num_features, num_features * 2, kernel_size=3, stride=2, padding=1
-                ),
-                ConvBlock(
-                    num_features * 2,
-                    num_features * 4,
-                    kernel_size=3,
-                    stride=2,
-                    padding=1,
-                ),
-            ]
-        )
-
-        self.residual_blocks = nn.Sequential(
-            *[ResidualBlock(num_features * 4) for _ in range(num_residuals)]
-        )
-
-        self.up_blocks = nn.ModuleList(
-            [
-                ConvBlock(
-                    num_features * 4,
-                    num_features * 2,
-                    down=False,
-                    kernel_size=3,
-                    stride=2,
-                    padding=1,
-                    output_padding=1,
-                ),
-                ConvBlock(
-                    num_features * 2,
-                    num_features * 1,
-                    down=False,
-                    kernel_size=3,
-                    stride=2,
-                    padding=1,
-                    output_padding=1,
-                ),
-            ]
-        )
-
-        self.last = nn.Conv2d(
-            num_features * 1,
-            img_channels,
-            kernel_size=7,
-            stride=1,
-            padding=3,
-            padding_mode="reflect",
+        upConvBlock = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=256,
+                out_channels=128,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(),
+            nn.ConvTranspose2d(
+                in_channels=128, out_channels=64, kernel_size=3, stride=2, padding=1
+            ),
+            nn.ConvTranspose2d(
+                in_channels=64, out_channels=1, kernel_size=6, stride=2, padding=2
+            ),
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=1,
+                kernel_size=7,
+                stride=1,
+                padding=3,
+            ),
+            nn.Tanh(),
         )
 
     def forward(self, x):
---
+++ selfAttentionBlock.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import sys
+
+# test functionality of view
+class testClass(nn.Module):
+    def __init__(self, in_dim):
+        super().__init__()
+        self.in_channel = in_dim
+
+        self.query_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+        )
+        self.key_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+        )
+        self.value_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim, kernel_size=1
+        )
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        batch_size, c, h, w = x.shape
+        # proj_query  = self.query_conv(x).view(batch_size,-1,w*h)
+        proj_query = self.query_conv(x).view(batch_size, -1, w * h).permute(0, 2, 1)
+        proj_key = self.key_conv(x).view(batch_size, -1, w * h)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        
+        proj_value = self.value_conv(x).view(batch_size, -1, w * h)
+        out = torch.bmm(proj_value,attention.permute(0,2,1) )
+        out = out.view(batch_size, c, w , h)
+        return out
+
+
+def testFunc():
+    x = torch.randn(3, 256, 32, 32)
+    simpleModel = testClass(in_dim=256)
+    print(simpleModel(x).shape)
+
+
+testFunc()
+
+
+# sys.exit()
+
+
+class SelfAttentionBlock(nn.Module):
+    """Self Attention Layer/Block"""
+
+    def __init__(self, in_dim):
+        super().__init__()
+        self.in_channel = in_dim
+
+        self.query_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+        )
+        self.key_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+        )
+        self.value_conv = nn.Conv2d(
+            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+        )
+
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        """
+        inputs:
+            x : input feature maps (B X C X W X H)
+        returns:
+            out : self attention value + input features
+            attention: B x N X N (N is Width*Height)
+        """
+
+        batch_size, c, h, w = x.shape
+        proj_query = self.query_conv(x).view(batch_size, -1, w * h).permute(0, 2, 1)
+        # proj_query with view is of shape (batch_size, c, w*h)
+        # proj_query with permute becomes batch_size, w*h, c), ie transpose of proj_query for matrix multiplication
+        proj_key = self.key_conv(x).view(batch_size, -1, w * h)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        
+        proj_value = self.value_conv(x).view(batch_size, -1, w * h)
+        out = torch.bmm(proj_value,attention.permute(0,2,1) )
+        out = out.view(batch_size, c, w , h)
