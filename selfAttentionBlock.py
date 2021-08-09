acf932ea02de106d1cd36016b933e170466bf949
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2021-08-09 09:18:48 +0530

    add self attention block

--- selfAttentionBlock.py
+++ selfAttentionBlock.py
@@ -2,44 +2,44 @@ import torch
 import torch.nn as nn
 import sys
 
-# test functionality of view
-class testClass(nn.Module):
-    def __init__(self, in_dim):
-        super().__init__()
-        self.in_channel = in_dim
+# # test functionality of view
+# class testClass(nn.Module):
+#     def __init__(self, in_dim):
+#         super().__init__()
+#         self.in_channel = in_dim
 
-        self.query_conv = nn.Conv2d(
-            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
-        )
-        self.key_conv = nn.Conv2d(
-            in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
-        )
-        self.value_conv = nn.Conv2d(
-            in_channels=in_dim, out_channels=in_dim, kernel_size=1
-        )
-        self.softmax = nn.Softmax(dim=-1)
+#         self.query_conv = nn.Conv2d(
+#             in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+#         )
+#         self.key_conv = nn.Conv2d(
+#             in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1
+#         )
+#         self.value_conv = nn.Conv2d(
+#             in_channels=in_dim, out_channels=in_dim, kernel_size=1
+#         )
+#         self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self, x):
-        batch_size, c, h, w = x.shape
-        # proj_query  = self.query_conv(x).view(batch_size,-1,w*h)
-        proj_query = self.query_conv(x).view(batch_size, -1, w * h).permute(0, 2, 1)
-        proj_key = self.key_conv(x).view(batch_size, -1, w * h)
-        energy = torch.bmm(proj_query, proj_key)
-        attention = self.softmax(energy)
-        
-        proj_value = self.value_conv(x).view(batch_size, -1, w * h)
-        out = torch.bmm(proj_value,attention.permute(0,2,1) )
-        out = out.view(batch_size, c, w , h)
-        return out
+#     def forward(self, x):
+#         batch_size, c, h, w = x.shape
+#         # proj_query  = self.query_conv(x).view(batch_size,-1,w*h)
+#         proj_query = self.query_conv(x).view(batch_size, -1, w * h).permute(0, 2, 1)
+#         proj_key = self.key_conv(x).view(batch_size, -1, w * h)
+#         energy = torch.bmm(proj_query, proj_key)
+#         attention = self.softmax(energy)
 
-
-def testFunc():
-    x = torch.randn(3, 256, 32, 32)
-    simpleModel = testClass(in_dim=256)
-    print(simpleModel(x).shape)
+#         proj_value = self.value_conv(x).view(batch_size, -1, w * h)
+#         out = torch.bmm(proj_value,attention.permute(0,2,1) )
+#         out = out.view(batch_size, c, w , h)
+#         return out
 
 
-testFunc()
+# def testFunc():
+#     x = torch.randn(3, 256, 32, 32)
+#     simpleModel = testClass(in_dim=256)
+#     print(simpleModel(x).shape)
+
+
+# testFunc()
 
 
 # sys.exit()
@@ -79,10 +79,18 @@ class SelfAttentionBlock(nn.Module):
         proj_query = self.query_conv(x).view(batch_size, -1, w * h).permute(0, 2, 1)
         # proj_query with view is of shape (batch_size, c, w*h)
         # proj_query with permute becomes batch_size, w*h, c), ie transpose of proj_query for matrix multiplication
-        proj_key = self.key_conv(x).view(batch_size, -1, w * h)
-        energy = torch.bmm(proj_query, proj_key)
-        attention = self.softmax(energy)
-        
-        proj_value = self.value_conv(x).view(batch_size, -1, w * h)
-        out = torch.bmm(proj_value,attention.permute(0,2,1) )
-        out = out.view(batch_size, c, w , h)
+        proj_key = self.key_conv(x).view(batch_size, -1, w * h)  # (batch_size, c, w*h)
+        energy = torch.bmm(proj_query, proj_key)  # (batch_size, w*h, w*h)
+        attention = self.softmax(energy)  # (batch_size, w*h, w*h)
+
+        proj_value = self.value_conv(x).view(
+            batch_size, -1, w * h
+        )  # (batch_size, c*8, w*h) c*8 = C
+        out = torch.bmm(
+            proj_value, attention.permute(0, 2, 1)
+        )  # (batch_size, c*8, w*h)
+        # attention is transposed for matrix mul
+        out = out.view(batch_size, c, w, h)
+
+        out = self.gamma * out + x
+        return out, attention
